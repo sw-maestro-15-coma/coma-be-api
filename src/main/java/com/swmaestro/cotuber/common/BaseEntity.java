72ec696c160775254d6bd3dd27642326ef629fa9
Octoping <mooner925@gmail.com>
2024-06-28 16:14:45 +0900

    feat: BaseEntity에 컬럼명 추가

--- src/main/java/com/swmaestro/cotuber/common/BaseEntity.java
+++ src/main/java/com/swmaestro/cotuber/common/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.swmaestro.cotuber.common;
 
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,8 +16,12 @@ public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id = 0L;
+
     @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
     @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 }
