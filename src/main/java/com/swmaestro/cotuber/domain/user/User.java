567a0094eb2bf72246a0b52754ac049fae2ab41d
Octoping <myc1365@naver.com>
2024-07-12 03:59:44 +0900

    feat: 스프링 시큐리티로 google 로그인, 회원가입 구현

--- src/main/java/com/swmaestro/cotuber/domain/user/User.java
+++ src/main/java/com/swmaestro/cotuber/domain/user/User.java
@@ -3,6 +3,7 @@ package com.swmaestro.cotuber.domain.user;
 import com.swmaestro.cotuber.common.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -18,8 +19,27 @@ public class User extends BaseEntity {
     private String nickname;
     @Column(name = "profile_image_url", length = 100)
     private String profileImageUrl;
+    private AuthRole role;
     @Column(name = "instagram_id", length = 20)
     private String instagramId;
     @Column(name = "tiktok_id", length = 20)
     private String tiktokId;
+
+    @Builder
+    public User(String email, String oAuthId, String nickname, String profileImageUrl) {
+        this.email = email;
+        this.oAuthId = oAuthId;
+        this.nickname = nickname;
+        this.profileImageUrl = profileImageUrl;
+        this.role = AuthRole.USER;
+    }
+
+    public static User of(OAuthUserInfo userInfo) {
+        return User.builder()
+                .email(userInfo.email())
+                .oAuthId(userInfo.oAuthId())
+                .nickname(userInfo.nickname())
+                .profileImageUrl(userInfo.profileImageUrl())
+                .build();
+    }
 }
