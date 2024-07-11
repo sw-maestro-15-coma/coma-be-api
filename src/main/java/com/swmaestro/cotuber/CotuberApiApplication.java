44b03403c1748edc99359fe75d144081bb4f4735
Octoping <myc1365@naver.com>
2024-07-12 03:57:13 +0900

    fix: JPA 설정이 잘못된 오류 수정

--- src/main/java/com/swmaestro/cotuber/CotuberApiApplication.java
+++ src/main/java/com/swmaestro/cotuber/CotuberApiApplication.java
@@ -2,12 +2,14 @@ package com.swmaestro.cotuber;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class CotuberApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CotuberApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CotuberApiApplication.class, args);
+    }
 
 }
