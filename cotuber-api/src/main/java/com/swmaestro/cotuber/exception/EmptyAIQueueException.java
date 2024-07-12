76445a2923b4cda41aaeacf723fa387cea9bdc59
choijaewon323 <casdfghjke@naver.com>
2024-07-12 21:45:43 +0900

    feat: AI 처리 큐 구현체 생성

---
+++ cotuber-api/src/main/java/com/swmaestro/cotuber/exception/EmptyAIQueueException.java
@@ -0,0 +1,7 @@
+package com.swmaestro.cotuber.exception;
+
+public class EmptyAIQueueException extends RuntimeException {
+    public EmptyAIQueueException(String message) {
+        super(message);
+    }
+}
