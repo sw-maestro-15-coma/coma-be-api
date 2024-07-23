251967240de09d605dc901330530385e1626f66e
Octoping <myc1365@naver.com>
2024-07-24 04:24:25 +0900

    refactor: 쓸모 없어진 mock api 제거

--- cotuber-api/src/main/java/com/swmaestro/cotuber/api/EndpointController.java
+++ cotuber-api/src/main/java/com/swmaestro/cotuber/api/EndpointController.java
@@ -6,11 +6,9 @@ import com.swmaestro.cotuber.dashboard.DashboardService;
 import com.swmaestro.cotuber.dashboard.dto.DashboardListResponseDto;
 import com.swmaestro.cotuber.shorts.ShortsService;
 import com.swmaestro.cotuber.shorts.dto.ShortsListResponseDto;
-import com.swmaestro.cotuber.shorts.ProgressState;
 import com.swmaestro.cotuber.video.VideoService;
 import com.swmaestro.cotuber.video.dto.VideoCreateRequestDto;
 import com.swmaestro.cotuber.video.dto.VideoCreateResponseDto;
-import com.swmaestro.cotuber.video.dto.VideoListResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -34,91 +32,6 @@ public class EndpointController {
 
     @Operation(summary = "대시보드의 동영상 목록 조회")
     @GetMapping(value = "/dashboard")
-    public List<VideoListResponseDto> getMockVideoList() {
-        return mockVideoList();
-    }
-
-    @Operation(summary = "유튜브 링크로 동영상 추가(추출)")
-    @PostMapping("/video")
-    public VideoCreateResponseDto mockUpdateVideo(@RequestBody VideoCreateRequestDto requestDto) {
-        return VideoCreateResponseDto.builder()
-                .id(0L)
-                .build();
-    }
-
-    @Operation(summary = "생성된 숏폼 데이터의 목록 조회")
-    @GetMapping("/shorts")
-    public List<ShortsListResponseDto> getMockShortsList() {
-        return mockShortsList();
-    }
-
-    private List<VideoListResponseDto> mockVideoList() {
-        final String mockLink = "https://www.youtube.com/watch?v=qgcoZpZvpfM";
-        final String mockTitle = "조미료를 잔뜩넣은 스테이크를 구워 보았습니다";
-
-        return List.of(
-                VideoListResponseDto.builder()
-                        .id(0)
-                        .youtubeUrl(mockLink)
-                        .title(mockTitle)
-                        .progress(ProgressState.COMPLETE)
-                        .build(),
-                VideoListResponseDto.builder()
-                        .id(1)
-                        .youtubeUrl(mockLink)
-                        .title(mockTitle)
-                        .progress(ProgressState.COMPLETE)
-                        .build(),
-                VideoListResponseDto.builder()
-                        .id(2)
-                        .youtubeUrl(mockLink)
-                        .title(mockTitle)
-                        .progress(ProgressState.COMPLETE)
-                        .build(),
-                VideoListResponseDto.builder()
-                        .id(3)
-                        .youtubeUrl(mockLink)
-                        .title(mockTitle)
-                        .progress(ProgressState.COMPLETE)
-                        .build()
-        );
-    }
-
-    private List<ShortsListResponseDto> mockShortsList() {
-        final String mockLink = "http://www.test.com/shorts";
-        final String mockThumbnail = "http://thumbnail.test.com";
-        final String mockTopTitle = "테스트 제목";
-
-        return List.of(
-                ShortsListResponseDto.builder()
-                        .id(0)
-                        .topTitle(mockTopTitle)
-                        .s3Url(mockLink + 0)
-                        .thumbnailUrl(mockThumbnail)
-                        .build(),
-                ShortsListResponseDto.builder()
-                        .id(1)
-                        .topTitle(mockTopTitle)
-                        .s3Url(mockLink + 1)
-                        .thumbnailUrl(mockThumbnail)
-                        .build(),
-                ShortsListResponseDto.builder()
-                        .id(2)
-                        .topTitle(mockTopTitle)
-                        .s3Url(mockLink + 2)
-                        .thumbnailUrl(mockThumbnail)
-                        .build(),
-                ShortsListResponseDto.builder()
-                        .id(3)
-                        .topTitle(mockTopTitle)
-                        .s3Url(mockLink + 3)
-                        .thumbnailUrl(mockThumbnail)
-                        .build()
-        );
-    }
-
-    @Operation(summary = "대시보드의 동영상 목록 조회")
-    @GetMapping(value = "/dashboard", headers = "X-API-VERSION=2")
     public List<DashboardListResponseDto> getVideoList() {
         final long userId = extractUserId();
 
@@ -126,7 +39,7 @@ public class EndpointController {
     }
 
     @Operation(summary = "유튜브 링크로 동영상 추가(추출)")
-    @PostMapping(value = "/video", headers = "X-API-VERSION=2")
+    @PostMapping(value = "/video")
     public VideoCreateResponseDto updateVideo(@RequestBody VideoCreateRequestDto createRequestDto) {
         final long userId = extractUserId();
 
@@ -134,7 +47,7 @@ public class EndpointController {
     }
 
     @Operation(summary = "생성된 숏폼 데이터의 목록 조회")
-    @GetMapping(value = "/shorts", headers = "X-API-VERSION=2")
+    @GetMapping(value = "/shorts")
     public List<ShortsListResponseDto> getShortsList() {
         final long userId = extractUserId();
 
