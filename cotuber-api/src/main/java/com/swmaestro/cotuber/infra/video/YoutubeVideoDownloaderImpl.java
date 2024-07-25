7cbcf110190a8eaa43b1818cac5b76a1c74880f3
choijaewon323 <casdfghjke@naver.com>
2024-07-25 13:41:26 +0900

    fix: youtubedownloader charset utf8 추가
    - 배포 및 테스트 필요

--- cotuber-api/src/main/java/com/swmaestro/cotuber/infra/video/YoutubeVideoDownloaderImpl.java
+++ cotuber-api/src/main/java/com/swmaestro/cotuber/infra/video/YoutubeVideoDownloaderImpl.java
@@ -7,13 +7,13 @@ import com.swmaestro.cotuber.video.YoutubeVideoDownloader;
 import com.swmaestro.cotuber.video.dto.VideoDownloadResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
-import java.util.Objects;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 @RequiredArgsConstructor
 @Component
@@ -32,10 +32,13 @@ public class YoutubeVideoDownloaderImpl implements YoutubeVideoDownloader {
             throw new VideoDownloadFailException("request body json 파싱에 실패했습니다");
         }
 
+        Charset utf8 = StandardCharsets.UTF_8;
+        MediaType mediaType = new MediaType("application", "json", utf8);
+
         final ResponseEntity<ResponseBody> response = restClient.post()
                 .uri(URI)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON)
+                .contentType(mediaType)
+                .accept(mediaType)
                 .body(jsonBody)
                 .retrieve()
                 .toEntity(ResponseBody.class);
