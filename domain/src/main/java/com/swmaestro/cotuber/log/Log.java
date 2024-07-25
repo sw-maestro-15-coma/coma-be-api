8faa3d61881f982658ddd128c1bc203e0e726c50
최재원 <46291115+choijaewon323@users.noreply.github.com>
2024-07-25 16:09:20 +0900

    Update domain/src/main/java/com/swmaestro/cotuber/log/Log.java
    Co-authored-by: Octoping <mooner925@gmail.com>

--- domain/src/main/java/com/swmaestro/cotuber/log/Log.java
+++ domain/src/main/java/com/swmaestro/cotuber/log/Log.java
@@ -3,5 +3,10 @@ package com.swmaestro.cotuber.log;
 import lombok.Builder;
 
 @Builder
-public record Log(Long userId, Long shortsId, ProgressContext progressContext, String message) {
+public record Log(
+    Long userId,
+    Long shortsId,
+    ProgressContext progressContext,
+    String message
+) {
 }
