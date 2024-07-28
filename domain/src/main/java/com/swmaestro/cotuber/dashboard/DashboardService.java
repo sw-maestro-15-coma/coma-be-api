1399499bdfccf1b3ad253f38c36aba105ea79c52
Octoping <mooner925@gmail.com>
2024-07-29 02:09:32 +0900

    refactor: dashboard 조회 로직을 스트림으로 변경

--- domain/src/main/java/com/swmaestro/cotuber/dashboard/DashboardService.java
+++ domain/src/main/java/com/swmaestro/cotuber/dashboard/DashboardService.java
@@ -4,7 +4,6 @@ import com.swmaestro.cotuber.dashboard.dto.DashboardListResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -14,15 +13,9 @@ public class DashboardService {
 
     public List<DashboardListResponseDto> getDashboard(final long userId) {
         final List<Dashboard> dashboards = dashboardRepository.findAllByUserId(userId);
-        final List<DashboardListResponseDto> results = new ArrayList<>();
-        dashboards.forEach(e -> results.add(
-                DashboardListResponseDto.builder()
-                        .shortsId(e.shortsId())
-                        .title(e.title())
-                        .youtubeUrl(e.youtubeUrl())
-                        .progressState(e.progressState())
-                        .build()
-        ));
-        return results;
+
+        return dashboards.stream()
+                .map(DashboardListResponseDto::new)
+                .toList();
     }
 }
