712c1ab70c067f3b4dbcd43685a598435365e736
Octoping <myc1365@naver.com>
2024-08-02 04:07:20 +0900

    refactor: dashboard를 얻어오는 쿼리를 jpa 인터페이스로 변경

--- storage/db/src/main/java/com/swmaestro/cotuber/dashboard/DashboardRepositoryImpl.java
+++ storage/db/src/main/java/com/swmaestro/cotuber/dashboard/DashboardRepositoryImpl.java
@@ -1,7 +1,5 @@
 package com.swmaestro.cotuber.dashboard;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.Query;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,21 +9,12 @@ import java.util.List;
 @RequiredArgsConstructor
 @Repository
 public class DashboardRepositoryImpl implements DashboardRepository {
-    private final EntityManager entityManager;
+    private final DashboardDtoRepository dashboardDtoRepository;
 
     @Transactional(readOnly = true)
     @Override
     public List<Dashboard> findAllByUserId(final long userId) {
-        final Query query =
-                entityManager.createQuery("""
-                                            SELECT new com.swmaestro.cotuber.dashboard.DashboardDto(s.id, v.title, v.youtubeUrl, s.progressState)
-                                            FROM ShortsEntity s
-                                            INNER JOIN VideoEntity v ON s.videoId = v.id
-                                            WHERE s.userId = :userId
-                                """)
-                        .setParameter("userId", userId);
-
-        final List<DashboardDto> results = (List<DashboardDto>) query.getResultList();
+        List<DashboardDto> results = dashboardDtoRepository.findAllByUserId(userId);
 
         return results.stream()
                 .map(DashboardDto::toDashboard)
