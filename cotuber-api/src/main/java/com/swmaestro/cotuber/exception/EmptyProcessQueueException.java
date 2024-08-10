cda217b8e384d4df1c571b817bac4bcba1da28f6
choijaewon323 <casdfghjke@naver.com>
2024-08-10 13:29:21 +0900

    refactor: 사용 안하는 예외 클래스 및 dto 제거

--- cotuber-api/src/main/java/com/swmaestro/cotuber/exception/EmptyProcessQueueException.java
+++
@@ -1,7 +0,0 @@
-package com.swmaestro.cotuber.exception;
-
-public class EmptyProcessQueueException extends RuntimeException {
-    public EmptyProcessQueueException(String message) {
-        super(message);
-    }
-}
--- cotuber-api/src/main/java/com/swmaestro/cotuber/exception/ShortsTopTitleGenerateFailException.java
+++
@@ -1,7 +0,0 @@
-package com.swmaestro.cotuber.exception;
-
-public class ShortsTopTitleGenerateFailException extends RuntimeException {
-    public ShortsTopTitleGenerateFailException(String message) {
-        super(message);
-    }
-}
