91769beb3ee3ce13a046f3b6b047cee6fb0f4d84
Octoping <mooner925@gmail.com>
2024-08-24 10:28:22 +0900

    refactor: 코드 제거

--- domain/src/main/java/com/swmaestro/cotuber/video/YoutubeUrlParseKeyStrategy.java
+++
@@ -1,9 +0,0 @@
-package com.swmaestro.cotuber.video;
-
-import java.net.URI;
-
-public interface YoutubeUrlParseKeyStrategy {
-    boolean isApplicable(URI youtubeUrl);
-
-    String getKey(URI youtubeUrl);
-}
