e64ab59a7d25e89a4e58533fd5df5349af869471
developerryou <developerryou@gmail.com>
2024-09-20 16:41:26 +0900

    REMOVE: legacy shorts edit point 삭제

--- domain/src/main/java/com/swmaestro/cotuber/shorts/edit/ShortsEditPointRepository.java
+++
@@ -1,8 +0,0 @@
-package com.swmaestro.cotuber.shorts.edit;
-
-import java.util.Optional;
-
-public interface ShortsEditPointRepository {
-    ShortsEditPoint save(ShortsEditPoint editPoint);
-    Optional<ShortsEditPoint> findById(long shortsEditPointId);
-}
