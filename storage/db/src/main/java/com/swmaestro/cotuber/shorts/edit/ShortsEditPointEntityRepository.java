b21f289a11a27f73657ca4a6db9dc56352d14323
developerryou <developerryou@gmail.com>
2024-09-24 20:49:22 +0900

    REMOVE: EditPoint 삭제

--- storage/db/src/main/java/com/swmaestro/cotuber/shorts/edit/ShortsEditPointEntityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.swmaestro.cotuber.shorts.edit;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ShortsEditPointEntityRepository extends JpaRepository<ShortsEditPointEntity, Long> {
-
-}
