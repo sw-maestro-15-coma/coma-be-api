495bc56079fc598b94dac74f751243051d82d4e8
developerryou <developerryou@gmail.com>
2024-10-01 16:47:51 +0900

    REMOVE: After 관련 로직 삭제

--- domain/src/main/java/com/swmaestro/cotuber/ai/dto/AIProcessMessageSubtitleResponse.java
+++
@@ -1,8 +0,0 @@
-package com.swmaestro.cotuber.ai.dto;
-
-import lombok.Builder;
-
-@Builder
-public record AIProcessMessageSubtitleResponse(long userVideoRelationId,
-                                               List<Subtitle> subtitle) {
-}
--- domain/src/main/java/com/swmaestro/cotuber/ai/dto/AIProcessMessageTitleResponse.java
+++
@@ -1,8 +0,0 @@
-package com.swmaestro.cotuber.ai.dto;
-
-import lombok.Builder;
-
-@Builder
-public record AIProcessMessageTitleResponse(long userVideoRelationId,
-                                            String title) {
-}
