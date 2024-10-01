fd66b2cc712a65b382c1db09b1e3ce342c3f2b77
developerryou <developerryou@gmail.com>
2024-10-02 00:40:57 +0900

    REMOVE: UserVideoRelation

--- domain/src/main/java/com/swmaestro/cotuber/userVideoRelation/dto/UserVideoRelationCreateRequestDto.java
+++
@@ -1,7 +0,0 @@
-package com.swmaestro.cotuber.userVideoRelation.dto;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public record UserVideoRelationCreateRequestDto(@JsonProperty("youtube_url") String youtubeUrl) {
-
-}
