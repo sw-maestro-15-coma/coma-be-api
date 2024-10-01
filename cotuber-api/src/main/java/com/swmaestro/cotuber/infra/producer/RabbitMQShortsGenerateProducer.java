89215aa617068f3354da8b5b115db284e1d02a33
developerryou <developerryou@gmail.com>
2024-10-01 16:49:14 +0900

    FIX: RabbitMQ producer 수정

--- cotuber-api/src/main/java/com/swmaestro/cotuber/infra/producer/RabbitMQShortsGenerateProducer.java
+++ cotuber-api/src/main/java/com/swmaestro/cotuber/infra/producer/RabbitMQShortsGenerateProducer.java
@@ -3,21 +3,21 @@ package com.swmaestro.cotuber.infra.producer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
-import com.swmaestro.cotuber.shorts.ShortsProcessProducer;
-import com.swmaestro.cotuber.shorts.dto.ShortsProcessMessageRequest;
+import com.swmaestro.cotuber.shorts.ShortsGenerateProducer;
+import com.swmaestro.cotuber.shorts.dto.ShortsGenerateMessageRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class RabbitMQShortsProcessProducer implements ShortsProcessProducer {
-    private static final String QUEUE_NAME = "shorts-process";
+public class RabbitMQShortsGenerateProducer implements ShortsGenerateProducer {
+    private static final String QUEUE_NAME = "shorts-generate";
 
     private final Connection connection;
     private final ObjectMapper objectMapper;
 
     @Override
-    public void send(ShortsProcessMessageRequest request) {
+    public void send(ShortsGenerateMessageRequest request) {
         try (Channel channel = connection.createChannel()) {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
@@ -25,7 +25,7 @@ public class RabbitMQShortsProcessProducer implements ShortsProcessProducer {
 
             channel.basicPublish("", QUEUE_NAME, null, message);
         } catch (Exception e) {
-            throw new IllegalStateException("shorts process producer에서 예외 발생 : " + e.getMessage());
+            throw new IllegalStateException("shorts generate producer에서 예외 발생 : " + e.getMessage());
         }
     }
 }
--- cotuber-api/src/main/java/com/swmaestro/cotuber/infra/producer/RabbitMQVideoSubtitleGenerateProducer.java
+++ cotuber-api/src/main/java/com/swmaestro/cotuber/infra/producer/RabbitMQVideoSubtitleGenerateProducer.java
@@ -3,21 +3,21 @@ package com.swmaestro.cotuber.infra.producer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
-import com.swmaestro.cotuber.draft.DraftAIProcessProducer;
-import com.swmaestro.cotuber.draft.dto.DraftAIProcessMessageRequest;
+import com.swmaestro.cotuber.video.VideoSubtitleGenerateProducer;
+import com.swmaestro.cotuber.video.dto.VideoSubtitleGenerateMessageRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class RabbitMQAIProcessProducer implements DraftAIProcessProducer {
-    private static final String QUEUE_NAME = "ai-process";
+public class RabbitMQVideoSubtitleGenerateProducer implements VideoSubtitleGenerateProducer {
+    private static final String QUEUE_NAME = "video-subtitle-generate";
 
     private final Connection connection;
     private final ObjectMapper objectMapper;
 
     @Override
-    public void send(DraftAIProcessMessageRequest request) {
+    public void send(VideoSubtitleGenerateMessageRequest request) {
         try (Channel channel = connection.createChannel()) {
             channel.queueDeclare(QUEUE_NAME, false, false, false, null);
 
@@ -25,7 +25,7 @@ public class RabbitMQAIProcessProducer implements DraftAIProcessProducer {
 
             channel.basicPublish("", QUEUE_NAME, null, message);
         } catch (Exception e) {
-            throw new IllegalStateException("ai process producer에서 예외 발생 : " + e.getMessage());
+            throw new IllegalStateException("video subtitle generate producer에서 예외 발생 : " + e.getMessage());
         }
     }
 }
