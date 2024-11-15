d71e8cb075437e9eae5d09217c764119a19fd615
Octoping <myc1365@naver.com>
2024-11-16 01:43:53 +0900

    fix: isRefreshTokenExist 에러 수정

--- domain/src/main/java/com/swmaestro/cotuber/token/RefreshTokenService.java
+++ domain/src/main/java/com/swmaestro/cotuber/token/RefreshTokenService.java
@@ -6,15 +6,13 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
-import static com.swmaestro.cotuber.DateUtil.toTimestamp;
-
 @RequiredArgsConstructor
 @Service
 public class RefreshTokenService {
     private final RefreshTokenRepository refreshTokenRepository;
 
     public boolean isRefreshTokenExists(long userId, String token, LocalDateTime now) {
-        return refreshTokenRepository.isRefreshTokenExists(userId, token, toTimestamp(now));
+        return refreshTokenRepository.isRefreshTokenExists(userId, token, now);
     }
 
     public void save(User user, String refreshToken, long expiresIn) {
